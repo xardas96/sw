48ed7f6d2baf6892f17648ed40addb117aa38bf9
Roman Sorokowski <xardas4400@gmail.com>
2013-11-16 21:17:19 +0100

    libki do kamerki

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,22 +2,35 @@ package main;
 
 import gui.MainFrame;
 
+import java.awt.Dimension;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 import javax.imageio.ImageIO;
 import javax.swing.SwingUtilities;
 
 import ar.DerivativeGaussianKernel;
 import ar.DesktopMarkerFinder;
+import ar.Marker;
+import ar.MarkerFinder;
+
+import com.github.sarxos.webcam.Webcam;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		InputStream is = new FileInputStream("londonTest.jpeg");
-		InputStream is2 = new FileInputStream("londonTest.jpeg");
+//		testMarkerFinder("fixedTest.png");
+		testCamera(new Dimension(176,144));
+	}
+	
+	private static void testMarkerFinder(String fileName) throws Exception {
+		InputStream is = new FileInputStream(fileName);
+		InputStream is2 = new FileInputStream(fileName);
 		DesktopMarkerFinder finder = new DesktopMarkerFinder();
 //		Image im = finder.drawEdgels(is);
 //		Image im = finder.drawLineSegments(is);
@@ -48,7 +61,22 @@ public class Main {
 				mf3.setVisible(true);
 			}
 		});
-//		MarkerFinder ir = new DesktopMarkerFinder();
-//		List<Marker> markers = ir.readImage(is);
+		MarkerFinder ir = new DesktopMarkerFinder();
+		List<Marker> markers = ir.readImage(is);
+	}
+	
+	private static void testCamera(Dimension cameraDimension) {
+		Webcam webcam = Webcam.getDefault();
+		webcam.open();
+		double start = System.currentTimeMillis();
+		BufferedImage img = webcam.getImage();
+		double stop = System.currentTimeMillis();
+		try {
+			ImageIO.write(img, "png", new File("test.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println(stop - start);
+		webcam.close();
 	}
 }
