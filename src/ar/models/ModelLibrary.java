8951c15fdc5b64490e11dc15c1a0d65423ac7649
Bollgar <bollgar@tlen.pl>
2014-01-08 22:25:29 +0100

    Dodanie do modellibrary stolu do 4

--- src/ar/models/ModelLibrary.java
+++ src/ar/models/ModelLibrary.java
@@ -13,6 +13,7 @@ public abstract class ModelLibrary {
 	public static void init() throws Exception {
 		modelMap = new HashMap<>();
 		modelMap.put(1, loadScene("obj\\airtable.obj"));
+		modelMap.put(4, loadScene("obj\\airtable.obj"));
 	}
 
 	public static BranchGroup getModel(int code) {
